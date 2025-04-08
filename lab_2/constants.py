f8656dde86465f90df3974504705aaec6c85a79f
ctwoth <muromec8906@gmail.com>
2025-04-08 20:29:08 +0400

    preparing for lab

--- lab_2/constants.py
+++
@@ -1,3 +0,0 @@
-DECODED_TEXT_PATH = "C:\\OIB_lb1\\isb_DI\\lab_2\\decoded.txt"
-TEXT_PATH = "C:\\OIB_lb1\\isb_DI\\lab_2\\text.txt"
-KEY_PATH = "C:\\OIB_lb1\\isb_DI\\lab_2\\key.txt"
--- lab_2/main.py
+++
@@ -1,7 +0,0 @@
-from constants import *
-
-def main():
-    print()
-
-if __name__ == "__main__":
-    main()
