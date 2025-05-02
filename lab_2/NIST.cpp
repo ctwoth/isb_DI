643544ccaea09fdc7650d4251cb40c33ebd9f139
ctwoth <muromec8906@gmail.com>
2025-05-02 20:23:43 +0400

    delete funk

--- lab_2/NIST.cpp
+++ lab_2/NIST.cpp
@@ -1,14 +1,6 @@
 #include "NIST.h"
 
 
-void print_test_result(ostream& stream, string test_name, double p_val) {
-    stream << "Test: " << test_name << '\n';
-    stream << "P-value: " << p_val << '\n';
-    stream << "Conclusion: " << ((p_val >= 0.01) ? "Passed" : "Failed");
-    stream << "\n\n";
-}
-
-
 double test1(string sequence) {
     double summ = 0;
 
