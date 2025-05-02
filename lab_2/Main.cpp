46971d731150d41c23959bdbb1a62919fbc426b7
ctwoth <muromec8906@gmail.com>
2025-05-02 16:50:55 +0400

    Update Main.cpp

---
+++ lab_2/Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include "NIST.h"
+#include "consts.h"
+
+using namespace std;
+
+void sequence_testing(string results_path, string sequence){
+	ofstream file;
+	file.open(results_path);
+
+	file << "testing sequence: " << sequence << "\n\n";
+
+	double rez1 = test1(sequence);
+	double rez2 = test2(sequence);
+	double rez3 = test3(sequence);
+
+	print_test_result(file, "Frequency bitwise test.", rez1);
+	print_test_result(file, "Identical consecutive bits test.", rez2);
+	print_test_result(file, "Longest sequence of units in a block test.", rez3);
+
+	file.close();
+}
+
+int main() {
+	sequence_testing(JAVA_RESULT_PATH, JAVA_SEQUENCE);
+	sequence_testing(CPP_RESULT_PATH, CPP_SEQUENCE);
+	
+	return 0;
+}
