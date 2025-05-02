dfa314ea9700f95f57d9c3858b2128ca017967f4
ctwoth <muromec8906@gmail.com>
2025-05-02 16:51:17 +0400

    Update NIST.h

--- lab_2/NIST.h
+++ lab_2/NIST.h
@@ -1,9 +1,10 @@
 #pragma once
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
-void print_test_result(ostream& stream, string test_name, int p_val, bool is_passed);
-int  test1(string sequence);
-int  test2(string sequence);
-int  test3(string sequence);
+void print_test_result(ostream& stream, string test_name, double p_val);
+double  test1(string sequence);
+double  test2(string sequence);
+double  test3(string sequence);
