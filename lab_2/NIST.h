4b3c9fe4e32b9f4daa8e6b9e81682300afb29b0b
ctwoth <muromec8906@gmail.com>
2025-05-02 20:23:21 +0400

    delete funk

--- lab_2/NIST.h
+++ lab_2/NIST.h
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void print_test_result(ostream& stream, string test_name, double p_val);
+
 double  test1(string sequence); // returning P-value
 double  test2(string sequence); // returning P-value
 double  test3(string sequence); // returning P-value
