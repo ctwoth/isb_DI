65c7c083cd3d3bcd0b46767a10babee0e054f5f3
ctwoth <muromec8906@gmail.com>
2025-05-02 15:19:02 +0400

    Update NIST.cpp, make test1

--- lab_2/NIST.cpp
+++ lab_2/NIST.cpp
@@ -10,7 +10,18 @@ void print_test_result(ostream& stream, string test_name, double p_val) {
 
 
 double test1(string sequence) {
-    return 0;
+    double summ = 0;
+
+    for (int i = 0; i < sequence.size(); ++i) {
+        if (sequence[i] == '1')
+            summ++;
+        else
+            summ--;
+    }
+
+    summ = summ / sqrt(sequence.size());
+
+    return erfc(fabs(summ) / sqrt(2));
 }
 
 
