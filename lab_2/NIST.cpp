27c47b03d82bdf0cf2ceb77e97fc926fdcbcf008
ctwoth <muromec8906@gmail.com>
2025-05-02 19:54:31 +0400

    end with NIST.cpp

--- lab_2/NIST.cpp
+++ lab_2/NIST.cpp
@@ -2,8 +2,8 @@
 
 
 void print_test_result(ostream& stream, string test_name, double p_val) {
-    stream << "Test:" << test_name;
-    stream << "P-value" << p_val;
+    stream << "Test:" << test_name << '\n';
+    stream << "P-value" << p_val << '\n';
     stream << "Conclusion:" << ((p_val >= 0.01) ? "Passed" : "Failed");
     stream << "\n\n";
 }
@@ -22,10 +22,12 @@ double test1(string sequence) {
     summ = summ / sqrt(sequence.size());
 
     return erfc(fabs(summ) / sqrt(2));
+
 }
 
 
-double test2(string sequence) {    double zeta = 0;
+double test2(string sequence) {
+    double zeta = 0;
     size_t seq_len = sequence.size();
 
     for (int i = 0; i < seq_len; ++i) {
@@ -50,5 +52,31 @@ double test2(string sequence) {    double zeta = 0;
 
 
 double test3(string sequence) {
-    return 0;
+    int v[] = { 0,0,0,0 };
+
+    for (int start = 0; start < sequence.size(); start += BLOCK_LENGTH) {
+        int max_len = 0, cur_len = 0;
+
+        for (int i = start; i < start + BLOCK_LENGTH; ++i) {
+            if (sequence[i] == '1')
+                cur_len++;
+
+            else {
+                max_len = max(max_len, cur_len);
+                cur_len = 0;
+            }
+        }
+
+        if (max_len <= 1) v[0]++;
+        else if (max_len == 2) v[1]++;
+        else if (max_len == 3) v[2]++;
+        else if (max_len >= 4) v[3]++;
+    }
+
+    double xi = 0;
+    for (int i = 0; i < 4; ++i) {
+        xi += pow(v[i] - 16 * PI[i], 2)/(16*PI[i]);
+    }
+
+    return boost::math::gamma_p(1.5, xi/2);
 }
