cedd6b846ec7a2ac7bcea507e58ec5c43b4bec71
ctwoth <muromec8906@gmail.com>
2025-05-31 03:26:35 +0400

    Update CryptoSystem.py

--- lab_3/CryptoSystem.py
+++ lab_3/CryptoSystem.py
@@ -1,13 +1,35 @@
 import symmetric
 import asymmetric
-
-def generate_key(key_len: int, symm_path: str, public_path: str, private_path: str) -> None:
-    a = 1
+import file_utils
 
 
-def encryption(text: str, symmetric_key: str, public_key: str, private_key: str, save_path: str) -> None:
-    b = 2
+def keys_generator(key_len: int, symm_path: str, public_path: str, private_path: str) -> None:
+    public_key, private_key = asymmetric.generate_keys()
+    sym_key =                  symmetric.generate_key(key_len)
+
+    asymmetric.key_serialization(public_path, public_key)
+    asymmetric.key_serialization(private_path, private_key)
+
+    encr_sym_key = asymmetric.encrypt(public_key, sym_key)
+
+    symmetric.key_serialization(symm_path, encr_sym_key)
 
 
-def decryption(text: str, symmetric_key: str, public_key: str, private_key: str, save_path: str) -> None:
-    c = 3
+def encryption(text_path: str, symm_key_path: str, private_key_path: str, save_path: str) -> None:
+    encrypted_symm_key = file_utils.load_from_txt(symm_key_path)
+    private_key =        file_utils.load_key(private_key_path)
+    text =               file_utils.load_from_txt(text_path)
+
+    symmetric_key = asymmetric.decrypt(private_key, encrypted_symm_key)
+
+    symmetric.encrypt(text, symmetric_key, save_path)
+
+
+def decrypt(text_path: str, symm_key_path: str, private_key_path: str, save_path: str) -> None:
+    encrypted_symm_key = file_utils.load_from_txt(symm_key_path)
+    private_key =        file_utils.load_key(private_key_path)
+    text =               file_utils.load_from_txt(text_path)
+
+    symmetric_key = asymmetric.decrypt(private_key, encrypted_symm_key)
+
+    symmetric.decrypt(text, symmetric_key, save_path)
