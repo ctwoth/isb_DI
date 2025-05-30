ad63573fc2e525d48f3fe5872e7dfb0b8875dec0
ctwoth <muromec8906@gmail.com>
2025-05-31 03:46:21 +0400

    fix CryptoSystem.py

--- lab_3/CryptoSystem.py
+++ lab_3/CryptoSystem.py
@@ -3,9 +3,9 @@ import asymmetric
 import file_utils
 
 
-def keys_generator(key_len: int, symm_path: str, public_path: str, private_path: str) -> None:
+def generate_keys(key_len: int, symm_path: str, public_path: str, private_path: str) -> None:
     public_key, private_key = asymmetric.generate_keys()
-    sym_key =                  symmetric.generate_key(key_len)
+    sym_key =                 symmetric.generate_key(key_len)
 
     asymmetric.key_serialization(public_path, public_key)
     asymmetric.key_serialization(private_path, private_key)
@@ -25,7 +25,7 @@ def encryption(text_path: str, symm_key_path: str, private_key_path: str, save_p
     symmetric.encrypt(text, symmetric_key, save_path)
 
 
-def decrypt(text_path: str, symm_key_path: str, private_key_path: str, save_path: str) -> None:
+def decryption(text_path: str, symm_key_path: str, private_key_path: str, save_path: str) -> None:
     encrypted_symm_key = file_utils.load_from_txt(symm_key_path)
     private_key =        file_utils.load_key(private_key_path)
     text =               file_utils.load_from_txt(text_path)
