08800f6c7db04d6e657b735ef438306a0b6abb30
ctwoth <muromec8906@gmail.com>
2025-05-31 15:41:27 +0400

    Update asymmetric.py

--- lab_3/asymmetric.py
+++ lab_3/asymmetric.py
@@ -1,13 +1,16 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+
 def generate_keys() -> (str, str):
     return 'public', 'private'
 
 
-def encrypt(text: str, public_key: str) -> str:
-    return 'encrypted text'
+def encrypt(text: str,public_key: rsa.RSAPublicKey) -> bytes:
+    return 'encrypted text'.encode('utf-8')
 
 
-def decrypt(text: str, private_key: str) -> str:
-    return 'decrypted text'
+def decrypt(text: str, private_key: rsa.RSAPrivateKey) -> bytes:
+    return 'decrypted text'.encode('utf-8')
 
 
 def key_serialization(key: str, save_path: str) -> None:
