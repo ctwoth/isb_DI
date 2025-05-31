7fc1c3d6bfd3d3a7ddac81c5c4ac58f64c095af0
ctwoth <muromec8906@gmail.com>
2025-05-31 05:30:00 +0400

    rebuild symmetric.py

--- lab_3/symmetric.py
+++ lab_3/symmetric.py
@@ -2,12 +2,12 @@ def generate_key() -> str:
     return 'key'
 
 
-def encrypt(text: str, key: str) -> str:
-    return 'encrypted text'
+def encrypt(text: str, key: str, save_path: str) -> None:
+    a = 1
 
 
-def decrypt(text: str, key: str) -> str:
-    return 'decrypted text'
+def decrypt(text: str, key: str, save_path: str) -> None:
+    a = 2
 
 
 def key_serialization(key: str, save_path: str) -> None:
