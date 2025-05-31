9f08f523388f4e2362de8096ddb85d40dcb6efb6
ctwoth <muromec8906@gmail.com>
2025-06-01 03:28:45 +0400

    Update CryptoSystem.py

--- lab_3/CryptoSystem.py
+++ lab_3/CryptoSystem.py
@@ -9,10 +9,10 @@ import file_utils
 
 def generate_keys(key_len: int, symm_path: str, public_path: str, private_path: str) -> None:
     public_key, private_key = asymmetric.generate_keys()
-    sym_key =                  symmetric.generate_key(key_len)
+    sym_key =                 symmetric.generate_key(key_len)
 
-    asymmetric.key_serialization(public_path, public_key)
-    asymmetric.key_serialization(private_path, private_key)
+    asymmetric.public_key_serialization(public_key, public_path)
+    asymmetric.private_key_serialization(private_key, private_path)
 
     encr_sym_key = asymmetric.encrypt(sym_key, public_key)
 
@@ -20,22 +20,22 @@ def generate_keys(key_len: int, symm_path: str, public_path: str, private_path: 
 
 
 def encryption(text_path: str, symm_key_path: str, private_key_path: str, save_path: str) -> None:
-    encrypted_symm_key = file_utils.load_from_txt(symm_key_path)
+    encrypted_symm_key = file_utils.load_bytes_from(symm_key_path)
     private_key =        load_pem_private_key(file_utils.load_bytes_from(private_key_path),None)
     text =               file_utils.load_from_txt(text_path)
 
     symmetric_key = asymmetric.decrypt(encrypted_symm_key, private_key)
 
-    encrypted_text = symmetric.encrypt(text, symmetric_key)
+    encrypted_text = symmetric.encrypt(text.encode('utf-8'), symmetric_key)
     file_utils.load_in_txt(encrypted_text.decode('utf-8'), save_path)
 
 
 def decryption(text_path: str, symm_key_path: str, private_key_path: str, save_path: str) -> None:
-    encrypted_symm_key = file_utils.load_from_txt(symm_key_path)
+    encrypted_symm_key = file_utils.load_bytes_from(symm_key_path)
     private_key =        load_pem_private_key(file_utils.load_bytes_from(private_key_path),None)
     text =               file_utils.load_from_txt(text_path)
 
     symmetric_key = asymmetric.decrypt(encrypted_symm_key, private_key)
 
-    decrypted_text = symmetric.decrypt(text, symmetric_key)
+    decrypted_text = symmetric.decrypt(text.encode('utf-8'), symmetric_key)
     file_utils.load_in_txt(decrypted_text.decode('utf-8'), save_path)
