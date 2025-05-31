59c8c20ef62be45268411b50cce8aa6698533971
ctwoth <muromec8906@gmail.com>
2025-06-01 03:50:30 +0400

    small fix asymmetric.py

--- lab_3/asymmetric.py
+++ lab_3/asymmetric.py
@@ -1,7 +1,6 @@
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.backends import default_backend
 
 import file_utils
 
