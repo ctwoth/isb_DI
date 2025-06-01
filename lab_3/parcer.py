bb1aea9ffc68342b419f36793b3c6be3215d1bdf
ctwoth <muromec8906@gmail.com>
2025-06-01 23:10:06 +0400

    delete useless argument in parcer.py

--- lab_3/parcer.py
+++ lab_3/parcer.py
@@ -14,7 +14,6 @@ def parse() -> argparse.Namespace:
     parser.add_argument("task",            type=str, help="Program task: generation / encryption / decryption")
     parser.add_argument("-s", "--settings",type=str, default="settings.json", help="Path to settings in JSON-file")
     parser.add_argument("-kb","--key_bits",type=int, default=192, help="3DES symmetric key length (64, 128 or 192)")
-    parser.add_argument("-uk","--user_key",type=str, edefault="", help="Path to the symmetric key file")
 
     args = parser.parse_args()
 
