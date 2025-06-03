aa7aff5bf886d83fe4d80801972068bdaee41e92
ctwoth <muromec8906@gmail.com>
2025-06-04 02:19:27 +0400

    Update main.py

--- lab_4/main.py
+++ lab_4/main.py
@@ -1,48 +1,63 @@
-import os
-
-from file_utils import FileUtils
-from parser import Parser
-from card_manager import CardManager
+import multiprocessing as mp
+import hashlib
+import tqdm
 
 
-def check_sets(sets: dict) -> None:
-    """
-    check what sets got all necessary params and files
-    :param sets{dict}:
-    :return None:
-    """
-    if not os.path.isfile(sets["save_path"]):
-        raise ValueError("Wrong path to initial file")
+class CardManager:
+    @staticmethod
+    def alg_luhn(card_num: str) -> bool:
+        total = 0
 
-    if len(sets["last_numbers"]) != 4:
-        raise ValueError("not 4 numbers")
+        for i, digit in enumerate(card_num):
+            num = int(digit)
+            if i % 2 == 0:
+                num *= 2
+                if num > 9:
+                    num -= (num // 10) + (num % 10)
 
-    if len(sets["hash"]) > 10:
-        raise ValueError("Hash doesn't look correct...")
+            total += num
 
-    if len(sets["bins"]) > 0:
-        raise ValueError("Empty bank BINs")
+        return total % 10 == 0
 
+    @staticmethod
+    def hashing_card(num: str) -> str:
+        return hashlib.sha224(num.encode()).hexdigest()
 
-def main() -> None:
-    try:
-        args = Parser.parse()
-        sets = FileUtils.load_from_json(args.settings)
-        check_sets(sets)
+    @staticmethod
+    def find_card_from_hash(bins: list[str], last_nums: str, target_hash: str, free_cores: int = mp.cpu_count()) -> str:
+        num_range = 10**(16 - 6 - len(last_nums))
+        core_range = num_range // free_cores
 
-        match args.task:
-            #case 'tests':
-                #will be soon...#
-            #case 'gui':
-                #will be soon...#
+        with mp.Pool(processes=free_cores) as p:
+            results = []
 
+            for card_bin in bins:
+                for iteration in range(free_cores):
+                    start = core_range * iteration
+                    end = core_range * (iteration + 1) if iteration != free_cores - 1 else num_range
 
-            case _:
-                raise ValueError("incorrect program task")
+                    results.append(p.apply_async(
+                        CardManager.hash_search,
+                        (card_bin, last_nums, start, end, target_hash)
+                        )
+                    )
 
-    except Exception as error:
-        print("Error!\n\t", error)
+            for i in range(len(results)):
+                r = results[i].get()
+                if r:
+                    p.terminate()
+                    return r
 
+        return None
 
-if __name__ == '__main__':
-    main()
+    @staticmethod
+    def hash_search(card_bin: str, last_nums: str, start: int, end: int, target_hash: str) -> str:
+        for middle_nums in range(start, end):
+            card = f"{card_bin}{middle_nums}{last_nums}"
+
+            rand_hash = CardManager.hashing_card(card)
+
+            if rand_hash == target_hash:
+                return card
+
+        return None
