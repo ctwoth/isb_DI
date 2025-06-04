99ca7b140f40f72d9b3e8fb4a24f55b6200a8544
ctwoth <muromec8906@gmail.com>
2025-06-04 05:27:09 +0400

    Update to PEP8 standart card_manager.py

--- lab_4/card_manager.py
+++ lab_4/card_manager.py
@@ -1,14 +1,18 @@
 import multiprocessing as mp
 import hashlib
-import tqdm
-
 
 class CardManager:
     @staticmethod
-    def alg_luhn(card_num: str) -> bool:
+    def alg_luhn(num_str: str) -> bool:
+        """
+        using Luhn test to prove what card_num correct
+
+        :param num_str:
+        :return correct num or not:
+        """
         total = 0
 
-        for i, digit in enumerate(reversed (card_num)):
+        for i, digit in enumerate(reversed (num_str)):
             num = int(digit)
             if i % 2 == 1:
                 num *= 2
@@ -21,10 +25,25 @@ class CardManager:
 
     @staticmethod
     def hashing_card(num: str) -> str:
+        """
+        getting hash of num using sha224 algorythm
+
+        :param num:
+        :return sha224_hash:
+        """
         return hashlib.sha224(num.encode()).hexdigest()
 
     @staticmethod
     def find_card_from_hash(bins: list[str], last_nums: str, target_hash: str, free_cores: int = mp.cpu_count()) -> str:
+        """
+        using BINs, and last numbers trying to find card number knowing its hash with direct iteration on multiple processes.
+
+        :param bins:
+        :param last_nums:
+        :param target_hash:
+        :param free_cores:
+        :return rinded_card:
+        """
         num_range = 10**(16 - 6 - len(last_nums))
         core_range = num_range // free_cores
 
@@ -52,6 +71,16 @@ class CardManager:
 
     @staticmethod
     def hash_search(card_bin: str, last_nums: str, start: int, end: int, target_hash: str) -> str:
+        """
+        direct iteration of card_nums in range from {start} to {end} numbers
+        
+        :param card_bin: 
+        :param last_nums: 
+        :param start: 
+        :param end: 
+        :param target_hash: 
+        :return founded_num:
+        """
         for middle_nums in range(start, end):
             card = f"{card_bin}{middle_nums}{last_nums}"
 
